f2099fcac3394eafc8208d7b71443d89e5d39f42
AnnaHtslk <hannah.htslk@gmail.com>
2024-03-28 20:31:16 +0200

    task4 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector('.login-form');
 
+const onFormSubmit = event => {
+    event.preventDefault();
+    
+
+    const data = {
+    email: event.target.elements.email.value.trim(),
+    password: event.target.elements.password.value.trim()
+}
+    if (data.email === '' || data.password === '') {
+        alert('All form fields must be filled in');
+        return;
+    }
+
+    console.log(data);
+    loginForm.reset();
+}
+
+loginForm.addEventListener('submit', onFormSubmit);
