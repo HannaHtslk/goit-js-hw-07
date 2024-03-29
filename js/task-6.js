dc083653f3766594c85a33dd0e679fc7ec78f83d
AnnaHtslk <hannah.htslk@gmail.com>
2024-03-29 16:49:26 +0200

    task6 started

--- js/task-6.js
+++ js/task-6.js
@@ -6,5 +6,23 @@ function getRandomHexColor() {
 
 const createBtn = document.querySelector('button[data-create]');
 const destroyBtn = document.querySelector('button[data-destroy]');
-const container = document.querySelector('boxes');
+const container = document.querySelector('#boxes');
+const input = document.querySelector('[type=number]');
 
+
+function createBox() {
+  const boxes = [];
+  const quantityBoxes = input.value;
+  
+  for (let i = 0; i < quantityBoxes; i++) {
+    const box = document.createElement('div');
+    boxes.push(box);
+  }
+
+  container.append(...boxes);
+  console.log(container);
+  return container;
+
+}
+
+createBtn.addEventListener('click', createBox);
